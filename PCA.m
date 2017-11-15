d737d2241bdd77e8385f5c46b8d0005d8f3206f5
Omar Kadhim <ok4117@arc01.doc.ic.ac.uk>
2017-11-15 19:02:19 +0000

    First files

---
+++ PCA.m
@@ -0,0 +1,22 @@
+function U = PCA(data)
+
+    X = data';
+    means = mean(X,2);
+    
+    %Centering data
+    
+    meanX = repmat(means,1,size(data,1));
+    X_cent = X-meanX;
+    [V,E] = eig(X_cent'*X_cent);
+    disp(diag(E))
+    
+    % Account for reduction in rank of one due to removal
+    % of mean previously
+    
+    E = E(2:size(E),2:size(E));
+    V = V(:,2:size(V,2));
+    
+    
+    U = X*V*E^(-0.5);
+    
+end
